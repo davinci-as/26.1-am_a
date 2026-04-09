a249184339f3311432fbb6831e9271db5d0ac1b0
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2026-04-08 22:26:33 -0300

    feat: add button on click event

--- app/src/main/java/ar/edu/davinci/clasesam/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/clasesam/MainActivity.java
@@ -3,6 +3,7 @@ package ar.edu.davinci.clasesam;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view){
-        Log.i("app-testing", "send message está funcionando");
+        Log.i("app-testing", "el click sobre la imagen está funcionando");
     }
 
     @Override
@@ -21,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        /* evento de click sobre botón */
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("app-testing", "el click sobre el botón está funcionando");
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
