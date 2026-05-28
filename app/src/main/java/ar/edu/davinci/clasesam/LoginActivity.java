3dca3e60627c5cdc68dbd4e6076b16773c445216
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2026-05-27 23:01:36 -0300

    feat: add login with firebase auth

---
+++ app/src/main/java/ar/edu/davinci/clasesam/LoginActivity.java
@@ -0,0 +1,53 @@
+package ar.edu.davinci.clasesam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+
+    public void login(View view) {
+        mAuth = FirebaseAuth.getInstance();
+        String email = ((EditText) findViewById(R.id.login_email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.login_password)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+
+                FirebaseUser user = authResult.getUser();
+                if(user == null) return;
+                Log.i("FirebaseAuth Login", user.getUid());
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/ar/edu/davinci/clasesam/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/clasesam/MainActivity.java
@@ -15,11 +15,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     public final  int BLOG_ID = 1;
     public final  int BLOG_DETAILS = 3;
 
     public final  int BLOG_UPDATE = 23;
+
+    FirebaseAuth mAuth;
 
 
     @Override
@@ -45,6 +52,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null ) {
+            Log.i("FirebaseAuth", currentUser.getUid());
+        } else {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
